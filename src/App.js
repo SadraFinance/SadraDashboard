53a048fcd6942b7f10263e34baf911d9df6e7d7d
Mohammad Masoudie <m.mh.itg@gmail.com>
2020-09-16 20:08:41 +0430

    Fix: Navbar fixed.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import './App.css';
-import SiderDemo from './components/nav';
+import { ConfigProvider } from 'antd';
 import './assets/style.css'
-import { Layout } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
+import './App.css';
+import BasicLayout from './layouts/BasicLayout';
+
+
 
 function App() {
   return (
-    <div className="App">
-       <Layout>
-      <Header>Header</Header>
-      <Layout>
-        <Content>Content</Content>
-        <Sider><SiderDemo/></Sider>
-      </Layout>
-      <Footer>Footer</Footer>
-    </Layout>
-    </div>
+    <ConfigProvider direction="rtl" className="App">
+     <BasicLayout/>
+    </ConfigProvider>
   );
 }
 
