938b3be1fea1407f4a3c0b91fa56d78a963ae335
Mohammad Masoudie <m.mh.itg@gmail.com>
2020-09-17 23:36:17 +0430

    Fix: Logo Added

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
+import { Layout, Menu, Typography, PageHeader } from "antd";
 import {
   LineChartOutlined,
   FundViewOutlined,
@@ -7,19 +7,22 @@ import {
   RadarChartOutlined,
   QuestionCircleOutlined,
 } from "@ant-design/icons";
-import logo from "../logo.svg";
+import logo from "../assets/logo.svg";
+
+const { Title } = Typography;
 
 const { Header, Content, Footer, Sider } = Layout;
-const { SubMenu } = Menu;
 
 class BasicLayout extends React.Component {
   state = {
     collapsed: false,
+    showLogoTitle: true,
   };
 
   onCollapse = (collapsed) => {
     console.log(collapsed);
     this.setState({ collapsed });
+    this.showLogoTitle = !this.showLogoTitle;
   };
 
   render() {
@@ -30,13 +33,14 @@ class BasicLayout extends React.Component {
           collapsed={this.state.collapsed}
           onCollapse={this.onCollapse}
         >
-          <img
-            style={{ padding: 8 }}
-            height="80px"
-            src={logo}
-            className="App-logo"
-            alt="logo"
-          />
+          <div className="logo">
+            <img alt="Sadra" src={logo} />
+            {this.showLogoTitle ? null : (
+              <Title style={{ color: "white", marginTop: "1rem" }} level={4}>
+                تحلیلگر صدرا
+              </Title>
+            )}
+          </div>
           <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
             <Menu.Item key="1" icon={<FundViewOutlined />}>
               نمای اصلی
@@ -44,22 +48,36 @@ class BasicLayout extends React.Component {
             <Menu.Item key="2" icon={<FundProjectionScreenOutlined />}>
               پورتفو
             </Menu.Item>
-            <Menu.Item key="2" icon={<RadarChartOutlined />}>
+            <Menu.Item key="3" icon={<RadarChartOutlined />}>
               استراتژی‌ها
             </Menu.Item>
-            <Menu.Item key="2" icon={<LineChartOutlined />}>
+            <Menu.Item key="4" icon={<LineChartOutlined />}>
               بک‌ تست
             </Menu.Item>
-            <Menu.Item key="3" icon={<QuestionCircleOutlined />}>
+            <Menu.Item key="5" icon={<QuestionCircleOutlined />}>
               راهنمای استراتژی
             </Menu.Item>
           </Menu>
         </Sider>
         <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            <PageHeader
+              className="site-page-header"
+              onBack={() => null}
+              title="Title"
+              subTitle="This is a subtitle"
+            />
+          </Header>
           <Content></Content>
           <Footer style={{ textAlign: "center" }}>
-            Ant Design ©2018 Created by Ant UED
+            Sadra Analysis Dashboard ©2021 Created by{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://sadrafinance.ir"
+            >
+              Sadra Group
+            </a>
           </Footer>
         </Layout>
       </Layout>
